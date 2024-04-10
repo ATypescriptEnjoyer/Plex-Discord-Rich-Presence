f0fc36fbb0b5058b1d40d8b4d7c81d99e6306ad0
Sasha Ryder <8694395+SashaRyder@users.noreply.github.com>
2024-04-10 22:29:42 +0100

    updated build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
-yarn build
-node --experimental-sea-config sea-config.json
-node -e "require('fs').copyFileSync(process.execPath, 'app.exe')"
-signtool remove /s app.exe
-npx postject app.exe NODE_SEA_BLOB sea-prep.blob --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2
+call yarn build
+call node --experimental-sea-config sea-config.json
+call node -e "require('fs').copyFileSync(process.execPath, 'app.exe')"
+call signtool remove /s app.exe
+call npx postject app.exe NODE_SEA_BLOB sea-prep.blob --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2

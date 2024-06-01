c2c97d7efa3ac417aea5a976d3eb09b2449c264a
sasharyder <8694395+SashaRyder@users.noreply.github.com>
2024-06-01 16:54:47 +0100

    Fixing ubuntu build

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 yarn build
 node --experimental-sea-config sea-config.json
-cp $(command -v node) app
+node -e "require('fs').copyFileSync(process.execPath, 'app')"
 npx --yes postject app NODE_SEA_BLOB sea-prep.blob \
     --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2 

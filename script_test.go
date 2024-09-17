77e2ace44a1e418558163eda61343b74fb113cb8
Sasha <8694395+ATypescriptEnjoyer@users.noreply.github.com>
2024-09-17 20:32:07 +0100

    Added tests

---
+++ script_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"strconv"
+	"testing"
+	"time"
+)
+
+func TestLoadEnv(t *testing.T) {
+	env, err := loadEnv()
+	if err != nil {
+		t.Errorf("loadEnv() returned error: %s", err)
+	}
+	if env.ClientID == "" || env.MQTTHost == "" || env.Username == "" || env.Password == "" || env.Topic == "" {
+		t.Errorf("loadEnv() did not load environment variables correctly")
+	}
+}
+
+func TestCalculateEnd(t *testing.T) {
+	offset := 1000
+	duration := 5000
+	offsetStr := strconv.Itoa(offset)
+	durationStr := strconv.Itoa(duration)
+	endTime := calculateEnd(offsetStr, durationStr)
+	expectedTime := time.Now().Add(time.Duration(duration-offset-1000) * time.Millisecond)
+	if endTime != expectedTime {
+		t.Errorf("calculateEnd(%s, %s) did not return correct end time", offsetStr, durationStr)
+	}
+}
+
+func TestHandleTopicMessage(t *testing.T) {
+	// This test is difficult to write because it depends on a real MQTT broker
+	// and the client login process. You might need to mock out some of these dependencies.
+}
